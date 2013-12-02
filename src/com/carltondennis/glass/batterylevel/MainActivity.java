f5aa0582fa99b7068345eca50376889cbc8b28b7
Alex Dennis <alex.dennis@gmail.com>
2013-12-02 11:03:28 -0500

    a little cleanup :/

--- src/com/carltondennis/glass/batterylevel/MainActivity.java
+++ src/com/carltondennis/glass/batterylevel/MainActivity.java
@@ -14,24 +14,24 @@ import com.google.android.glass.app.Card;
 public class MainActivity extends Activity {
 	
 	private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
-	    @Override
-	    public void onReceive(Context c, Intent i) {
-	    	int level 		 = i.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+		@Override
+		public void onReceive(Context c, Intent i) {
+			int level 		 = i.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
 			int scale 		 = i.getIntExtra(BatteryManager.EXTRA_SCALE, -1);			
 			float batteryPct = (level / (float)scale) * 100;
-	        
+
 			Card card = new Card(c);
 			card.setText("Battery Level: " + Float.toString(batteryPct) + "%");
 			View cardView = card.toView();
 			setContentView(cardView);
-	    }
+		}
 	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		registerReceiver(mBatInfoReceiver, new IntentFilter(
-	            Intent.ACTION_BATTERY_CHANGED));		
+		        Intent.ACTION_BATTERY_CHANGED));		
 	}
 }
