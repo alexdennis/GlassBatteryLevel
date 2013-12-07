c1f45dc3358e517e23465fa453a0da834fb42050
Alex Dennis <alex.dennis@gmail.com>
2013-12-07 15:59:04 -0500

    Adding text to speech. Glass says battery level in addition to showing it

--- src/com/carltondennis/glass/batterylevel/MainActivity.java
+++ src/com/carltondennis/glass/batterylevel/MainActivity.java
@@ -2,21 +2,26 @@ package com.carltondennis.glass.batterylevel;
 
 import android.app.Activity;
 import android.view.View;
+import android.os.BatteryManager;
+import android.os.Bundle;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.BroadcastReceiver;
-import android.os.BatteryManager;
+import android.content.res.Resources;
+import android.speech.tts.TextToSpeech;
 
 import com.google.android.glass.app.Card;
 
 import java.text.DecimalFormat;
 
 public class MainActivity extends Activity {
+	private boolean mSpoken = false;
 	
 	private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context c, Intent i) {
+			Resources res 	   = getResources();
 			int level          = i.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
 			int scale          = i.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
 			double levelDouble = Integer.valueOf(level).doubleValue();
@@ -24,12 +29,39 @@ public class MainActivity extends Activity {
 			double batteryFrac = levelDouble / scaleDouble;
 			DecimalFormat df   = new DecimalFormat("#%");
 			
+			String levelText   = 
+					res.getString(R.string.battery_level_label) 
+					+ " " 
+					+ df.format(batteryFrac);
+			
+			// Speak it.
+			if (!mSpoken) {
+				// This is to avoid repetition
+				mSpeech.speak(levelText, TextToSpeech.QUEUE_FLUSH, null);
+				mSpoken = true;
+			}
+			
+			// Display it.
 			Card card = new Card(c);
-			card.setText("Battery Level: " + df.format(batteryFrac));
+			card.setText(levelText);
 			View cardView = card.toView();
 			setContentView(cardView);
 		}
 	};
+	
+	private TextToSpeech mSpeech;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        mSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                // Do nothing.
+            }
+        });
+	}
 
 	@Override
 	protected void onStart() {
@@ -42,5 +74,13 @@ public class MainActivity extends Activity {
 	protected void onStop() {
 		super.onStop();
 		unregisterReceiver(mBatInfoReceiver);
+	}
+	
+	@Override
+    public void onDestroy() {
+		mSpeech.shutdown();
+        mSpeech = null;
+        
+        super.onDestroy();
 	}
 }
